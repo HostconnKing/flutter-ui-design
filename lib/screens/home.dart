94d948c431b630fb8d6860b2e9ed0c502522abf7
King Romero <hostconn.king@gmail.com>
2023-09-01 08:24:35 +0800

    Featured games background
    1. I created private function _featuredGameWidget to hold the network image, but i use SizedBox here,
    2. I added two var to get the size of the device height and width using MediaQuery of context
    3. I use BoxFit.cover to fit the network image in the screen.
    4. I added PageView inside the SizedBox and it will inherit the size of the sizedbox.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_sedign/data.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,10 +9,41 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  var _deviceHeight;
+  var _deviceWidth;
+
+  Widget _featuredGameWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth,
+      child: PageView(
+          scrollDirection: Axis.horizontal,
+          children: featuredGames.map((game) {
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(game.coverImage.url),
+                ),
+              ),
+            );
+          }).toList()),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-
+      body: Stack(
+        children: <Widget>[_featuredGameWidget()],
+      ),
     );
   }
 }
