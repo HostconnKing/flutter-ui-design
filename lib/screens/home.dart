b10c3ee596079d7771f248ae644bd6c74836ca2b
King Romero <hostconn.king@gmail.com>
2023-09-01 11:42:03 +0800

    Background gradient
    1. I created new private function _gradientBoxWidget to create the fade gradient effect.
    2.  I  use align to bottom center to position ng gradient to the bottom center. because by default its position at the top.
    3. stops property is to set the start to end of gradient, from  first color(RGBO) to colors transparent.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,26 +9,53 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  var _deviceHeight;
-  var _deviceWidth;
+  var _deviceHeight = 0.0;
+  var _deviceWidth = 0.0;
 
   Widget _featuredGameWidget() {
     return SizedBox(
       height: _deviceHeight * 0.50,
       width: _deviceWidth,
       child: PageView(
-          scrollDirection: Axis.horizontal,
-          children: featuredGames.map((game) {
-            return Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(game.coverImage.url),
-                ),
+        scrollDirection: Axis.horizontal,
+        children: featuredGames.map((game) {
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(game.coverImage.url),
               ),
-            );
-          }).toList()),
+            ),
+          );
+        }).toList(),
+      ),
     );
+  }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight * 0.80,
+        width: _deviceWidth,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(35, 45, 59, 1.0),
+              Colors.transparent,
+            ],
+            stops: [0.65, 1.0],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _topLayerWidget(){
+  
+
   }
 
   @override
@@ -42,7 +69,11 @@ class _MainScreenState extends State<MainScreen> {
     _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
-        children: <Widget>[_featuredGameWidget()],
+        children: <Widget>[
+          _featuredGameWidget(),
+          _gradientBoxWidget(),
+          _topLayerWidget(),
+        ],
       ),
     );
   }
